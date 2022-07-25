140dcc097a9877b8e312a189036023f74a5f5f95
thitiwat-t <85502719+thitiwat-t@users.noreply.github.com>
2022-07-26 01:26:44 +0700

    🔧fix: no 🆂🥚🥚 🗣

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,22 +1,19 @@
 <script>
   export let counter = 0
-  const audio = new Audio();
+  
   const segg = () => {
     addOneSeg();
     uwoh();
   }
-  const uwoh = async () => {
-    audio.src = "../../../public/segg.wav";
+  const uwoh = () => {
+    const audio = new Audio();
+    audio.src = "/segg.wav";
     audio.play();
   }
   const addOneSeg = () => {
     counter += 1;
   }
 </script>
-
-<audio preload="auto">
-  <source src="../../../public/segg.wav" type="audio/wav">
-</audio>
 
 <button on:click={segg}>
   😭🗣
