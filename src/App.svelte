14f86354f028088460aefa9ed21040b06c0514ad
thitiwat-t <85502719+thitiwat-t@users.noreply.github.com>
2022-07-26 01:05:27 +0700

    🎉feat: 😭 i n i t 🥚

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Counter from './lib/Counter.svelte'
+  let count = 0;
+  $: seg = count;
+</script>
+
+<main>
+  <h1>😭🆂🥚</h1>
+
+  <div class="card">
+    <Counter bind:counter={count}/>
+  </div>
+  <p>
+    🆂🥚 : {seg}
+  </p>
+</main>
+
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let counter = 0
+  const audio = new Audio();
+  const segg = () => {
+    addOneSeg();
+    uwoh();
+  }
+  const uwoh = async () => {
+    audio.src = "../../../public/segg.wav";
+    audio.play();
+  }
+  const addOneSeg = () => {
+    counter += 1;
+  }
+</script>
+
+<audio preload="auto">
+  <source src="../../../public/segg.wav" type="audio/wav">
+</audio>
+
+<button on:click={segg}>
+  😭🗣
+</button>
