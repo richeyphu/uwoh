2e998888bec9f757f3084b1d2cabcd137aacab0f
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2022-07-29 03:07:55 +0700

    🎉feat: 😭 world will remember yours 🥚

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,35 +1,44 @@
 <script lang="ts">
-  export let counter = 0
-  
+  export let counter = 0;
+
   const segg = () => {
-    addOneSeg();
+    addOneSegg();
     uwoh();
     saveSegg();
-  }
+    countSegg();
+  };
   const uwoh = () => {
     const audio = new Audio();
     audio.src = "/segg.wav";
     audio.play();
-  }
-  const addOneSeg = () => {
+  };
+  const addOneSegg = () => {
     counter += 1;
-  }
+  };
   const saveSegg = () => {
     if (typeof window !== "undefined") {
       localStorage.setItem("seg", counter.toString());
     }
-  }
-  const loadSegg = () => {
+  };
+  (function loadSegg() {
     if (typeof window !== "undefined") {
       const stored = localStorage.getItem("seg");
       if (stored) {
         counter = parseInt(stored);
       }
     }
-  }
-  loadSegg();
+  })();
+  const countSegg = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.countapi.xyz/hit/uwoh.vercel.app/segg");
+    // (() => {
+    //   xhr.responseType = "json";
+    //   xhr.onload = () => {
+    //     alert(`This button has been clicked ${this.response.value} times!`);
+    //   };
+    // })();
+    xhr.send();
+  };
 </script>
 
-<button on:click={segg}>
-  😭🗣
-</button>
+<button on:click={segg}> 😭🗣 </button>
